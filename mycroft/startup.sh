b81030bf9e2d464eaccda4911030c5ccd6e704f7
PrimusNZ <primus@primusnz.live>
2022-10-22 02:06:44 +1300

    Bootstrapping

--- mycroft/startup.sh
+++ mycroft/startup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 source /opt/mycroft/.venv/bin/activate
 
-if [ ! -d /data ]; then
-    mkdir /data
-fi
-
-if [ ! -f /data/mycroft.conf ]; then
-    cp /etc/mycroft/mycroft.conf /data/
+if [ ! -d /data/config ]; then
+    cp -av /var/cache/mycroft/* /data/
 fi
 
 /opt/mycroft/./start-mycroft.sh all
