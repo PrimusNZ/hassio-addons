edea4c83ec59ff752a88b1b8d4101723ad5db5dd
PrimusNZ <primus@primusnz.live>
2022-10-22 00:58:41 +1300

    Updates

--- mycroft/startup.sh
+++ mycroft/startup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 source /opt/mycroft/.venv/bin/activate
+
+if [ ! -d /data ]; then
+    mkdir /data
+fi
+
+if [ ! -f /data/mycroft.conf ]; then
+    cp /etc/mycroft/mycroft.conf /data/
+fi
+
 /opt/mycroft/./start-mycroft.sh all
 
 tail -f /var/log/mycroft/*.log
