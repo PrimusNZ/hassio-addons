cc81cd4958b264adf46443fdb02ef2514d161abb
PrimusNZ <primus@primusnz.live>
2022-10-22 21:41:03 +1300

    MyCroft Updates

--- mycroft/startup.sh
+++ mycroft/startup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 source /opt/mycroft/.venv/bin/activate
 
-if [ ! -d /data/config ]; then
-    cp -av /var/cache/mycroft/* /data/
+if [ ! -d /data/conf ]; then
+    mkdir -p /data/conf
+    cp /etc/mycroft/mycroft.conf /data/conf/
+fi
+
+if [ ! -d /data/local ]; then
+    mkdir -p /data/local
+    /opt/mycroft/.venv/bin/mimic3 --preload-voice en_UK/apope_low
 fi
 
 /opt/mycroft/./start-mycroft.sh all
